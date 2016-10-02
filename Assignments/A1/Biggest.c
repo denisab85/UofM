f0d85c6c30acf7551849540ba66974618f5aaddf
denisab85 <denisab85@gmail.com>
2016-10-02 14:41:56 -0500

    Assignment 1: checked compliance with code requirements

--- Assignments/A1/Biggest.c
+++ Assignments/A1/Biggest.c
@@ -4,6 +4,7 @@
 // COURSE: COMP 2160, SECTION: A02
 // INSTRUCTOR: Michael Zapp
 // ASSIGNMENT: 1
+// QUESTION: 1
 //
 // REMARKS: remake of Biggest.java
 //
@@ -19,30 +20,45 @@ const int MAXLEN = 256;
 int testsExecuted = 0;
 int testsFailed = 0;
 
-int charcat (char *string, char ch)
+
+// add a char at the end of the string
+int charCat (char *string, char ch);
+
+// find the longest word in the string
+int *longestWord(char *string, char *result);
+
+// test the longestWord function and verify results
+void testLongestWord(char *line, char *expected);
+
+
+int charCat (char *string, char ch)
 {
-    size_t cur_len = strlen(string);
-    if(cur_len < 254)
+    int charCat = 0;
+    size_t cur_len = strlen (string);
+    
+    // ensure the input string length does not exceed MAXLEN-1
+    if (cur_len < MAXLEN - 1)
     {
         string[cur_len] = ch;
         string[cur_len+1] = '\0';
     }
     else
-        return 1;
-    return 0;
+        charCat = 1;
+    return charCat;
 }
 
-int *longestWord(char *string, char *result)
+
+int *longestWord (char *string, char *result)
 {
-    int pos = 0;
+    int  pos = 0;
     char longest[MAXLEN] = "";
-    int maxLength = 0;
+    int  maxLength = 0;
     char current[MAXLEN] = "";
-    int currentLength = 0;
+    int  currentLength = 0;
     char ch = '\0';
     char line[MAXLEN] = "";
     
-    strcpy(line, string);
+    strcpy (line, string);
     strcat (line, " ");
     while (pos < strlen(line))
     {
@@ -51,25 +67,25 @@ int *longestWord(char *string, char *result)
             isalpha (line[pos - 1]) &&
             isalpha (line[pos + 1]))
         {
-            charcat (current, ch);
-        }
-        else if (isalnum(ch))
-        {
-            
-            charcat (current, ch);
-            currentLength++;
+            charCat (current, ch);
         }
         else
-        {
-            if (currentLength > maxLength)
+            if (isalnum (ch))
             {
-                strcpy (longest, current);
-                maxLength = currentLength;
+                charCat (current, ch);
+                currentLength++;
             }
-            
-            strcpy(current, "");
-            currentLength = 0;
-        }
+            else
+            {
+                if (currentLength > maxLength)
+                {
+                    strcpy (longest, current);
+                    maxLength = currentLength;
+                }
+                
+                strcpy (current, "");
+                currentLength = 0;
+            }
         
         pos++;
     }
@@ -79,71 +95,72 @@ int *longestWord(char *string, char *result)
 }
 
 
-void testLongestWord(char *line, char *expected)
+void testLongestWord (char *line, char *expected)
 {
     char result[MAXLEN] = "";
-    longestWord(line, result);
+    longestWord (line, result);
     
-    if (0 == strcmp(result, expected))
+    if (0 == strcmp (result, expected))
     {
-        printf("Passed: '%s' from '%s'\n", result, line);
+        printf ("Passed: '%s' from '%s'\n", result, line);
     }
     else
     {
-        printf("FAILED: '%s' instead of '%s' from '%s'\n", result, expected, line);
+        printf ("FAILED: '%s' instead of '%s' from '%s'\n", result, expected, line);
         testsFailed++;
     }
     testsExecuted++;
 }
 
 
-int main(int argc, const char * argv[]) {
-    printf("Testing typical cases, including punctuation\n\n");
-    testLongestWord("the quick brown foxes jumped over the lazy dogs", "jumped");
-    testLongestWord("hello world she said", "hello");
-    testLongestWord("Hello\tworld\tshe\tsaid", "Hello");
-    testLongestWord("HELLO, world she said", "HELLO");
-    testLongestWord("hello world! she said???", "hello");
-    testLongestWord("\"hello world!\", she said.", "hello");
-    testLongestWord("easy as abc123", "abc123");
-    testLongestWord("easy as abc,123", "easy");
+int main (int argc, const char * argv[])
+{
+    printf ("Testing typical cases, including punctuation\n\n");
+    testLongestWord ("the quick brown foxes jumped over the lazy dogs", "jumped");
+    testLongestWord ("hello world she said", "hello");
+    testLongestWord ("Hello\tworld\tshe\tsaid", "Hello");
+    testLongestWord ("HELLO, world she said", "HELLO");
+    testLongestWord ("hello world! she said???", "hello");
+    testLongestWord ("\"hello world!\", she said.", "hello");
+    testLongestWord ("easy as abc123", "abc123");
+    testLongestWord ("easy as abc,123", "easy");
     
-    printf("\nTesting empty cases\n\n");
-    testLongestWord("", "");
-    testLongestWord("!", "");
-    testLongestWord(" ", "");
-    testLongestWord("\t", "");
-    testLongestWord("      ", "");
-    testLongestWord("# $ ? % !", "");
+    printf ("\nTesting empty cases\n\n");
+    testLongestWord ("", "");
+    testLongestWord ("!", "");
+    testLongestWord (" ", "");
+    testLongestWord ("\t", "");
+    testLongestWord ("      ", "");
+    testLongestWord ("# $ ? % !", "");
     
-    printf("\nTesting edge cases\n\n");
-    testLongestWord("a", "a");
-    testLongestWord("abc", "abc");
-    testLongestWord("abc d e f ghi", "abc");
-    testLongestWord("a a b cc dd abc", "abc");
-    testLongestWord("\"a a b cc dd abc.\"", "abc");
+    printf ("\nTesting edge cases\n\n");
+    testLongestWord ("a", "a");
+    testLongestWord ("abc", "abc");
+    testLongestWord ("abc d e f ghi", "abc");
+    testLongestWord ("a a b cc dd abc", "abc");
+    testLongestWord ("\"a a b cc dd abc.\"", "abc");
     
-    printf("\nTesting apostrophes and dashes\n\n");
-    testLongestWord("this isn't five chars", "chars");
-    testLongestWord("this should've been eight chars said the computer", "should've");
-    testLongestWord("'this should've been eight chars', said the computer", "should've");
-    testLongestWord("'hello world!', she said softly.", "softly");
-    testLongestWord("topsy-turvy is a tenletter word", "topsy-turvy");
-    testLongestWord("topsy-turvy should not be incorrectly eleven characters", "incorrectly");
-    testLongestWord("---in-between-these---", "in-between-these");
-    testLongestWord("---in---between---these---", "between");
-    testLongestWord("here-is-an-edge-case but a muchmuchlongerword", "muchmuchlongerword");
-    testLongestWord("d-o-n't-g-o-o-v-e-r-t-h-e-e-d-g-e with muchmuchlongerwords", "muchmuchlongerwords");
-    testLongestWord("two=five-3 isn't three", "three");
+    printf ("\nTesting apostrophes and dashes\n\n");
+    testLongestWord ("this isn't five chars", "chars");
+    testLongestWord ("this should've been eight chars said the computer", "should've");
+    testLongestWord ("'this should've been eight chars', said the computer", "should've");
+    testLongestWord ("'hello world!', she said softly.", "softly");
+    testLongestWord ("topsy-turvy is a tenletter word", "topsy-turvy");
+    testLongestWord ("topsy-turvy should not be incorrectly eleven characters", "incorrectly");
+    testLongestWord ("---in-between-these---", "in-between-these");
+    testLongestWord ("---in---between---these---", "between");
+    testLongestWord ("here-is-an-edge-case but a muchmuchlongerword", "muchmuchlongerword");
+    testLongestWord ("d-o-n't-g-o-o-v-e-r-t-h-e-e-d-g-e with muchmuchlongerwords", "muchmuchlongerwords");
+    testLongestWord ("two=five-3 isn't three", "three");
     
-    printf("\nThese tests will be opposite in the C version\n\n");
-    testLongestWord("the word antidisestablishmentarianism is very long but not as long as 'Llanfairpwllgwyngyllgogerychwyrndrobwyll-llantysiliogogogoch'.", "Llanfairpwllgwyngyllgogerychwyrndrobwyll-llantysiliogogogoch");
-    testLongestWord("the word antidisestablishmentarianism is very long but not as long as 'Llanfairpwllgwyngyllgogerychwyrndrobwyll-llantysiliogogogoch'.", "antidisestablishment");
-    testLongestWord("Java strings may contain \0 in the interior", "interior");
-    testLongestWord("C strings cannot contain \0 in the interior", "strings");
+    printf ("\nThese tests will be opposite in the C version\n\n");
+    testLongestWord ("the word antidisestablishmentarianism is very long but not as long as 'Llanfairpwllgwyngyllgogerychwyrndrobwyll-llantysiliogogogoch'.", "Llanfairpwllgwyngyllgogerychwyrndrobwyll-llantysiliogogogoch");
+    testLongestWord ("the word antidisestablishmentarianism is very long but not as long as 'Llanfairpwllgwyngyllgogerychwyrndrobwyll-llantysiliogogogoch'.", "antidisestablishment");
+    testLongestWord ("Java strings may contain \0 in the interior", "interior");
+    testLongestWord ("C strings cannot contain \0 in the interior", "strings");
     
-    printf("\nTotal number of tests executed: %d\n", testsExecuted);
-    printf("Number of tests passed:         %d\n", (testsExecuted - testsFailed));
-    printf("Number of tests failed:         %d\n", testsFailed);
+    printf ("\nTotal number of tests executed: %d\n", testsExecuted);
+    printf ("Number of tests passed:         %d\n", (testsExecuted - testsFailed));
+    printf ("Number of tests failed:         %d\n", testsFailed);
     return 0;
 }
--- Assignments/A1/Ruler.c
+++ Assignments/A1/Ruler.c
@@ -4,6 +4,7 @@
 // COURSE: COMP 2160, SECTION: A02
 // INSTRUCTOR: Michael Zapp
 // ASSIGNMENT: 1
+// QUESTION: 2
 //
 // REMARKS: remake of Ruler.java
 //
@@ -15,16 +16,24 @@
 #include <ctype.h>
 #include <assert.h>
 #include <math.h>
-#include <errno.h>
-#include <limits.h>
+
 
 const int MAXLEN = 256;
 
-int rule(int left, int right, int height, int theRuler[])
+
+int    rule (int left, int right, int height, int theRuler[]);
+double log2 (double val);
+int    printRuler (int length);
+
+// checks if the string represents a valid integer (positive/negative)
+int    isInt (const char *string);
+
+
+int rule (int left, int right, int height, int theRuler[])
 {
-    assert(left <= right);
-    assert(right >= 0);
-    assert(height >= 0);
+    assert (left <= right);
+    assert (right >= 0);
+    assert (height >= 0);
     
     int mid = 0;
     
@@ -38,8 +47,8 @@ int rule(int left, int right, int height, int theRuler[])
         mid = (right+left)/2;
         theRuler[mid] = height;
         
-        rule(left,mid,height-1, theRuler);
-        rule(mid,right,height-1, theRuler);
+        rule (left,mid,height-1, theRuler);
+        rule (mid,right,height-1, theRuler);
     }
     
     return 0;
@@ -48,61 +57,71 @@ int rule(int left, int right, int height, int theRuler[])
 
 double log2(double val)
 {
-    assert(val > 0);
+    assert (val > 0);
     return (log(val)/log(2));
 }
 
 
-int printRuler(int length)
+int printRuler (int length)
 {
-    assert(length > 0);
+    assert (length > 0);
     
     int theRuler[length+1];
-    int i;
+    int i = 0;
     
     for (i = 0; i <=length; i++)
     {
         theRuler[i] = 0;
     }
     
-    rule(0,length,(int)(log2(length)),theRuler);
+    rule (0, length, (int)(log2(length)), theRuler);
     
     for (i = 0; i <=length; i++)
     {
-        printf("%d", theRuler[i]);
+        printf ("%d", theRuler[i]);
     }
-    printf("\n");
+    printf ("\n");
     
     for (i = 0; i <=length; i++)
     {
-        printf("%d", i % 10);
+        printf ("%d", i % 10);
     }
-    printf("\n");
+    printf ("\n");
     return 0;
 }
 
 
-int isNum(const char *string) {
-    size_t length = strlen (string);
+int isInt (const char *string)
+{
     int i = 0;
+    int isNum = 1;
+    size_t length = strlen (string);
+    
+    // check the first character (it can be "-" or "+" or a digit)
     if ( (string[0] != '-') && (string[0] != '+') && (! isdigit(string[0])) )
-        return 0;
-    for (i=1; i < length; i++)
+        isNum = 0;
+    
+    // the rest should all be digits
+    i = 1;
+    while ( (isNum != 0) && (i < length) )
+    {
         if (! isdigit(string[i]))
-            return 0;
-    return 1;
+            isNum = 0;
+        i++;
+    }
+    return isNum;
 }
 
 
-int main(int argc, const char * argv[])
+int main (int argc, const char * argv[])
 {
     char done = 0;
-    int length;
+    int  length = 0;
     char inputLine[MAXLEN] = "";
     
     do
     {
-        printf("Enter a power of 2, which is at least 2, or q to quit: ");
+        printf ("Enter a power of 2, which is at least 2, or q to quit: ");
         if (fgets(inputLine, sizeof(inputLine), stdin))
         {
             inputLine[strlen(inputLine)-1] = '\0';
@@ -112,34 +131,35 @@ int main(int argc, const char * argv[])
                 {
                     done = 1;
                 }
-                else if ( ! isNum(inputLine) )
-                {
-                    printf("Hey, that's not a number!\n");
-                }
                 else
-                {
-                    length = atoi(inputLine);
-                    if ( (length < 2) || (ceil(log2(length)) != (int)(log2(length)) ) )
+                    if ( ! isInt(inputLine) )
                     {
-                        printf("Please be sure to enter a power of 2 that is >= 2.\n");
+                        printf ("Hey, that's not an integer!\n");
                     }
                     else
                     {
-                        printRuler(length);
+                        length = atoi(inputLine);
+                        if ( (length < 2) || (ceil(log2(length)) != (int)(log2(length)) ) )
+                        {
+                            printf ("Please be sure to enter a power of 2 that is >= 2.\n");
+                        }
+                        else
+                        {
+                            printRuler (length);
+                        }
                     }
-                }
             }
         }
         else
         {
-            clearerr(stdin);
-            printf("\n");
+            clearerr (stdin);
+            printf ("\n");
         }
         
     }
     while (!done);
     
-    printf("End of Processing\n");
+    printf ("End of Processing\n");
     return 0;
 }
 
